68edafab0da61fb1865ed63b2dcb9aabea88dc82
aliravn <alira.vn@gmail.com>
2019-09-27 13:59:56 +0200

    update connection

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 // this will avoid mysql_connect() deprecation error.
 error_reporting( ~E_DEPRECATED & ~E_NOTICE );
 
-$connect = new mysqli ('localhost','root','Deadend6!','cr13_valeria_nazarova_people');
+$connect = new mysqli ('localhost','root','','cr13_valeria_nazarova_people');
 
 if ($connect->connect_errno) {
    die("Failed to connect to MySQL: " . $connect->connect_error);
