91dd96c4066236408210da1c2a9af03abedc605d
aliravn <alira.vn@gmail.com>
2019-09-27 12:06:33 +0200

    change db-request for object-oriented

--- username_check.php
+++ username_check.php
@@ -3,8 +3,8 @@ require_once "db_connect.php";
 
 if (isset($_POST["username_match"])) {
 	$username_match = mysqli_real_escape_string($connect, $_POST["username_match"]);
-	$query = "SELECT * FROM users WHERE username = '".$username_match."'";
-	$result = mysqli_query($connect, $query);
+	$sql_request = "SELECT * FROM users WHERE username = '".$username_match."'";
+	$result = $connect->query($sql_request);
 	echo mysqli_num_rows($result);
 }
 ?>
