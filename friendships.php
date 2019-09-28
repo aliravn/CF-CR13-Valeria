ae254389bd0ce0b0a90fd027d474f6ab205a53f3
aliravn <alira.vn@gmail.com>
2019-09-28 15:29:45 +0200

    add 3 query to display users based on request status and from/to status

--- friendships.php
+++ friendships.php
@@ -11,9 +11,19 @@ $sql_user = "SELECT * FROM users WHERE userID=".$_SESSION['user'];
 $result = $connect->query($sql_user);
 $user_details = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-// display all friends of the current user
-$sql_friends = "SELECT userID, username, userpic FROM friendships JOIN users ON fk_userID_from = userID OR fk_userID_to = userID WHERE fk_userID_from =" .$_SESSION['user']. " AND userID !=" .$_SESSION['user']. " OR fk_userID_to =" .$_SESSION['user']. " AND userID !=".$_SESSION['user'];
+// display all friends of the current user, who confirmed request
+$sql_friends = "SELECT userID, username, userpic FROM friendships JOIN users ON fk_userID_from = userID OR fk_userID_to = userID WHERE friend_status='confirmed' AND fk_userID_from =" .$_SESSION['user']. " AND userID !=" .$_SESSION['user']. " OR friend_status='confirmed' AND fk_userID_to =" .$_SESSION['user']. " AND userID !=".$_SESSION['user'];
 $result_friends = $connect->query($sql_friends);
+
+
+// display all friends of the current user, whom user has sent request to
+$sql_request_sent = "SELECT userID, username, userpic FROM users JOIN friendships ON fk_userID_to = userID AND friend_status = 'request' WHERE fk_userID_from =" .$_SESSION['user'];
+$result_request_sent = $connect->query($sql_request_sent);
+
+// display friend requests, waiting for confirmation from current user
+$sql_friend_request = "SELECT userID, username, userpic, friendshipID FROM users JOIN friendships ON fk_userID_from = userID AND friend_status='request' WHERE fk_userID_to =" .$_SESSION['user'];
+$result_friend_request = $connect->query($sql_friend_request);
+
 ?>
 
 <!DOCTYPE html>
@@ -42,7 +52,7 @@ $result_friends = $connect->query($sql_friends);
 				<?php
 				if($result_friends->num_rows > 0) {
 					$name_of_user = strtoupper($user_details['username']);
-					echo "<h4 class='col-12 text-center'>THESE ARE FRIENDS OF <span>$name_of_user</span></h4>";
+					echo "<h4 class='col-12 text-center'><span>CONFIRMED</span> FRIENDS OF <span>$name_of_user</span></h4>";
 					echo "<div class='col-12 text-center' id='update'></div>";
 					while($row = $result_friends->fetch_assoc()) {
 						echo
@@ -54,11 +64,46 @@ $result_friends = $connect->query($sql_friends);
 						</div>";
 					}
 				} else {	
-					echo "<h4 class='col-12 text-center'>YOU HAVE NO FRIENDS YET</h4>";
+					echo "<h4 class='col-12 text-center'>YOU HAVE NO FRIENDS</h4>";
 					echo "<h4 class='col-12 text-center'>=^.^=</h4>";
 				}
 				?>
 			</div>
+			<div class="row">
+				<?php
+				if($result_request_sent->num_rows > 0) {
+					echo "<h4 class='col-12 text-center'>SENT REQUESTS TO:</h4>";
+					while($row = $result_request_sent->fetch_assoc()) {
+						echo
+						"<div class='col-6 col-md-3 col-lg-2 p-2  friend-card'>
+							<div class='friend-card col-border col-border-pending p-2'>
+								<img class='img-fluid img-thumbnail' src=".$row['userpic'].">
+								<p class='friend-username'>".$row['username']."</p>
+							</div>
+						</div>";
+					}
+				}
+				?>
+			</div>
+			<div class="row">
+				<?php
+				if($result_friend_request->num_rows > 0) {
+					echo "<h4 class='col-12 text-center'>WAITING FOR YOUR CONFIRMATION:</h4>";
+					while($row = $result_friend_request->fetch_assoc()) {
+						echo
+						"<div class='col-6 col-md-3 col-lg-2 p-2  friend-card'>
+							<div class='friend-card col-border col-border-pending p-2'>
+								<img class='img-fluid img-thumbnail' src=".$row['userpic'].">
+								<p class='friend-username'>".$row['username']."</p>
+							</div>
+							<a class='button-container'>	
+								<button class='add-friend-button' id='".$row['friendshipID']."' type='button'>Confirm</button>
+							</a>
+						</div>";
+					}
+				}
+				?>
+			</div>			
 		</div>
 	</div>
 
